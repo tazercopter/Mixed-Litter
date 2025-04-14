591efa1fc9023383dda76ef88af8f424078b3d9d
MikulDev <gamer3mc@gmail.com>
2025-04-14 16:00:57 -0400

    Fix unintentionally changed "variants" data attachment ID

--- src/main/java/dev/tazer/mixed_litter/MLDataAttachmentTypes.java
+++ src/main/java/dev/tazer/mixed_litter/MLDataAttachmentTypes.java
@@ -11,7 +11,7 @@ public class MLDataAttachmentTypes {
     public static final DeferredRegister<AttachmentType<?>> ATTACHMENT_TYPES = DeferredRegister.create(NeoForgeRegistries.ATTACHMENT_TYPES, MixedLitter.MODID);
 
     public static final Supplier<AttachmentType<String>> MOB_VARIANTS = ATTACHMENT_TYPES.register(
-            "mob_variant", () -> AttachmentType.builder(() -> "").serialize(Codec.STRING).build()
+            "mob_variants", () -> AttachmentType.builder(() -> "").serialize(Codec.STRING).build()
     );
     public static final Supplier<AttachmentType<String>> SUB_VARIANT = ATTACHMENT_TYPES.register(
             "sub_variant", () -> AttachmentType.builder(() -> "").serialize(Codec.STRING).build()
